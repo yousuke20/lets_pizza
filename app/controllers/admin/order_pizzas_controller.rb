1fba2c9a72d5d92ef8fe2f8ee9b94bf3db5673ea
Githubのユーザー名 <Github>
2021-03-04 02:53:56 +0000

    注文履歴詳細完了

--- app/controllers/admin/order_pizzas_controller.rb
+++ app/controllers/admin/order_pizzas_controller.rb
@@ -2,24 +2,24 @@ class Admin::OrderPizzasController < ApplicationController
   before_action :authenticate_admin!
   
   def update
-    @order_pizza = OrderPizza.find(params[:id])
+    @order_pizzas = OrderPizza.find(params[:id])
     @order = @order_pizza.order
-    @order_pizza.update(order_pizza_params)
+    @order_pizzas.update(order_pizza_params)
     
   # 1つの調理ステータスを「調理完了」にすると、 注文ステータスが「配達準備中」に更新される処理
-    if @order_pizza.cooking_status == "調理完了"
+    if @order_pizzas.cooking_status == "調理完了"
       @order.update(order_status: "配達準備中")
       flash[:success] = "調理ステータスを「調理完了」に、注文ステータスを「配達準備中」に変更しました！"
-      redirect_to admin_order_path(@order_pizza.order.id)
+      redirect_to admin_order_path(@order_pizzas.order.id)
   # 全ての調理ステータスを「調理完了」にすると、 注文ステータスが「配達準備完了」に更新される処理
     elsif @order.order_pizza.count == @order.order_pizza.where(cooking_status: "調理完了").count
       @order.update(order_status: "配達準備完了")
       flash[:success] = "全ての調理ステータスを「調理完了」に、注文ステータスを「配達準備完了」に変更しました！"
-      redirect_to admin_order_path(@order_pizza.order.id)
+      redirect_to admin_order_path(@order_pizzas.order.id)
   # 上記2つ以外の場合
     else
       flash[:success] = "調理ステータスを更新しました！"
-      redirect_to admin_order_path(@order_pizza.order.id)
+      redirect_to admin_order_path(@order_pizzas.order.id)
     end
   end
   
