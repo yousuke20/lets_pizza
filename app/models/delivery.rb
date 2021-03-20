44724efb598ecc6c4f5372292abba5c5b8d15c98
Githubのユーザー名 <Github>
2021-03-20 05:49:11 +0000

    model deliveries&orders edit

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,7 +3,7 @@ class Delivery < ApplicationRecord
 
   validates :member_id, :name, :postal_code, :address, :telephone_number, presence: true
   validates :postal_code, length: { minimum: 7}
-  validates :telephone_number, length: { minimum: 10, maximum: 11}
+  validates :telephone_number, length: { minimum: 10}
   # enumによる支払方法の管理
   enum payment_method: { 現金: 0, クレジットカード: 1, 銀行振込: 2 }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :member
 
   validates :member_id, :delivery_postal_code, :delivery_address, :delivery_name, :delivery_telephone_number, :billing_amount, :payment_method, presence: true
-
+  validates :delivery_telephone_number, length: { minimum: 10 }
   # enumによる支払方法、注文ステータスの管理
   enum payment_method: { 現金: 0, クレジットカード: 1, 銀行振込: 2 }
   enum order_status: { 入金待ち: 0, 入金確認: 1, 配達準備中: 2, 配達準備完了: 3, 配達中: 4 }
